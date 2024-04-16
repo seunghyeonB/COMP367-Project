d4a0b2c44b7292a0368794adc49d4b74a0915d62
seunghyeon ban <sban6@my.centennialcollege.ca>
2024-04-16 13:20:36 -0400

    project for comp367 from comp308

--- react-client/src/components/Home.jsx
+++ react-client/src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home(props)
 
     return (
         <div>
-            <h2> Dahye Lee - Lab Assignment1</h2>
+            <h2> Seunghyeon Ban - Lab Assignment1</h2>
             <p>React front-end calls Express REST API to add, 
             list, update, or delete a student, create an course, etc.</p>
         </div>
